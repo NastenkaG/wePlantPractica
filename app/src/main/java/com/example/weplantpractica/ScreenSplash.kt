8b010aa804dab8cb312f8bee808c7c2ba2f1de9b
guzno <anastasia.guznova@yandex.ru>
2021-12-10 11:58:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/weplantpractica/ScreenSplash.kt
@@ -0,0 +1,11 @@
+package com.example.weplantpractica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScreenSplash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
