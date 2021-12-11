0742b57992f861a2a6b6e109a4e4d1bac1a56ae2
guzno <anastasia.guznova@yandex.ru>
2021-12-11 21:14:54 +0300

    day2

--- app/src/main/java/com/example/weplantpractica/SplashScreen.kt
+++ app/src/main/java/com/example/weplantpractica/SplashScreen.kt
@@ -3,15 +3,16 @@ package com.example.weplantpractica
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.weplantpractica.databinding.SplashScreenBinding
+import com.example.weplantpractica.databinding.ActivitySplashScreenBinding
 
-class ScreenSplash : AppCompatActivity() {
-    lateinit var binding: SplashScreenBinding
+class SplashScreen : AppCompatActivity() {
+    lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.splashButton.setOnClickListener {
-            val intent = Intent(this, ScreenMenu::class.java)
+            val intent = Intent(this, MenuScreen::class.java)
             startActivity(intent)
         }
     }
